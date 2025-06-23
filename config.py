6558f54513177699db2a069f0238546502640342
ajpsyk <ajpsyk@gmail.com>
2025-06-23 14:19:41 -0700

    refactored for organization, easier to add more pages

---
+++ config.py
@@ -0,0 +1,16 @@
+# Canvas settings
+DPI = 150
+PAGE_WIDTH_INCHES = 8.5
+PAGE_HEIGHT_INCHES = 11
+PAGE_WIDTH = int(PAGE_WIDTH_INCHES * DPI)
+PAGE_HEIGHT = int(PAGE_HEIGHT_INCHES * DPI)
+
+# Margins (shared by all pages)
+TOP_MARGIN = int(0.125 * DPI)
+BOTTOM_MARGIN = int(0.25 * DPI)
+SIDE_MARGIN = int(0.25 * DPI)
+
+# Default paths
+FONT_PATH = "fonts/Dekko-Regular.ttf"
+DEFAULT_IMAGE_FOLDER = "Images/"
+DEFAULT_OUTPUT_PATH = "output/preview_grid.pdf"
