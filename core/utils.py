c9157940ca6bc482acd2b7a18d7602124809734b
ajpsyk <ajpsyk@gmail.com>
2025-08-05 17:40:17 -0700

    pdfmerger deprecated, replaced with pdfwriter/reader

--- core/utils.py
+++ core/utils.py
@@ -1,3 +1,6 @@
+from pypdf import PdfWriter, PdfReader
+import fitz  # PyMuPDF
+
 def draw_debug_box(draw, x0, y0, x1, y1, color="red", thickness=1):
     """
     Draws a rectangle outline for visual debugging.
@@ -34,3 +37,22 @@ def fit_text_to_width(text, font_loader, max_width, initial_size, min_size=8):
         text_width = font.getbbox(text)[2] - font.getbbox(text)[0]
 
     return font
+
+def merge_pdfs(output_path, pdf_paths):
+    writer = PdfWriter()
+    for path in pdf_paths:
+        reader = PdfReader(path)
+        for page in reader.pages:
+            writer.add_page(page)
+    with open(output_path, "wb") as f_out:
+        writer.write(f_out)
+
+def get_resolution_from_pdf():
+    doc = fitz.open("my_pdf.pdf")
+
+    for page in doc:
+        for img in page.get_images(full=True):
+            xref = img[0]
+            pix = fitz.Pixmap(doc, xref)
+            print(f"Image: {pix.width}x{pix.height} px")
+
